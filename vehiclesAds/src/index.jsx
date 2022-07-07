c509e1152b87af75f933ffe9d4e9820e56b1c35d
Carlos Tavares <carlostsa10@gmail.com>
2022-07-07 19:28:05 -0300

    initial commit and organization of all

---
+++ vehiclesAds/src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RouteOne from "./routes";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouteOne />
+  </React.StrictMode>
+);
